8cfaecc0c7f8b8fd0042a52eca9a70b2b1240cdc
kevinderitis <kevin.deritis@hotmail.com>
2022-09-07 22:19:16 -0300

    Dotenv example

---
+++ desafio4.js
@@ -0,0 +1,12 @@
+require('dotenv').config()
+
+const modo = process.env.MODO ?? 'prod'
+const puerto = Number(process.env.PUERTO ?? 0)
+const debug = process.env.DEBUG == 'true' ? true : false
+
+
+console.log({
+    modo,
+    puerto,
+    debug
+})
